a0540eb85e72fe935ba6f7e91e9070637c931afc
K1CH15 <118368300+K1CH15@users.noreply.github.com>
2024-01-23 10:57:02 -0500

    dominio

--- base/settings.py
+++ base/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-ac3ry3hntr5(lg86t5%%9wwxznr6+6fa1dj*=l5ee%r)!!k!%k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['sena.monguarte.com']
 
 # Application definition
 
